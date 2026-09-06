3d5473fc4c592c4b6c9e10f9b1f9b8b069ccfdaa
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-09-06 15:17:46 +0700

    Create main.js

---
+++ appScript/02_module/chuNha/dashboard/main.js
@@ -0,0 +1,41 @@
+function chuNha_buildDashboard(tongQuan) 
+    {
+        return `
+            <h1 class="card__caption">Trang chủ</h1>
+
+            <div class="luoi-tong-quan">
+                ${taoTheSo('Tổng số khu nhà', tongQuan.tongSoKhuNha || 0)}
+                ${taoTheSo('Tổng số phòng', tongQuan.tongSoPhong || tongQuan.tongPhong || 0)}
+                ${taoTheSo('Số phòng đang thuê', tongQuan.soPhongDangThue || tongQuan.dangThue || 0)}
+                ${taoTheSo('Số phòng trống', tongQuan.soPhongTrong || tongQuan.phongTrong || 0)}
+                ${taoTheSo('Tổng số khách', tongQuan.tongSoKhach || tongQuan.tongKhach || 0)}
+            </div>
+        `;
+    }
+
+    function capNhatTongQuanTuCache()
+    {
+        google.script.run
+            .withSuccessHandler(function(tongQuan)
+            {
+                APP.data.tongQuan = tongQuan || {};
+
+                if (
+                    APP.state.manHinhHienTai.vaiTro === 'chuNha' &&
+                    APP.state.manHinhHienTai.manHinh === 'home'
+                )
+                {
+                    $('#dashboard_div').innerHTML = chuNha_buildDashboard(APP.data.tongQuan);
+                }
+            })
+            .withFailureHandler(function(loi)
+            {
+                console.error(loi);
+            })
+            .sv_chuNha_getDashboardData(APP.user.token);
+    }
+
+    function hienThiGiaoDienChuNha() 
+    {
+        APP.ui.setManHinh('chuNha', 'home');
+    }
