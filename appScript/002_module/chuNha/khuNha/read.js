815810426968d4d44e55520a94e6f17750cd74b2
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-06 16:36:26 +0700

    dds

--- appScript/002_module/chuNha/khuNha/read.js
+++ appScript/002_module/chuNha/khuNha/read.js
@@ -5,14 +5,13 @@
             return;
         }
 
-        $('#danhSachKhuNha_div').innerHTML = `
-            
+        $('#danhSachKhuNha_div').innerHTML = `            
                 <div style="width: 100%;display: flex; flex-wrap: nowrap; justify-content: space-between; gap: 0.6rem;">
                     <span class="card__caption">Danh sách khu/tòa nhà</span>
                     <div class="menu__button" style="width: 160px;" onclick="themMoiKhuNha_lamMoi();hide('danhSachKhuNha_div');show('themMoiKhuNha_form', 'grid');">Khu nhà mới</div>
                 </div>
                 <div class="card">
-                    <table class="bang">
+                    <table class="bang" id="chuNha_danhSachKhuNha_table">
                         <thead>
                             <tr>
                                 <th>STT</th>
@@ -22,29 +21,37 @@
                             </tr>
                         </thead>
                         <tbody>
-                            ${danhSach.map(function(dong, index)
-                            {
-                                let trangThaiArray = {'dangHoatDong': 'Hoạt động', 'tamDung': 'Ngừng hoạt động'};
-                                if (dong.active == '1')
-                                {
-                                    return `
-                                        <tr ondblclick="khuNha_edit('${escapeHtml(dong.maKhuNha)}')">
-                                            <td>${index + 1}</td>
-                                            <td>${escapeHtml(dong.tenKhuNha || '')}</td>
-                                            <td>${escapeHtml(dong.diaChi || '')}</td>
-                                            <td>${escapeHtml(trangThaiArray[dong.trangThai] || '')}</td>
-                                            <td>
-                                                <div class="deleteButton"
-                                                    onclick="event.stopPropagation(); xoaKhuNha('${escapeHtml(dong.maKhuNha)}', '${escapeHtml(dong.tenKhuNha || '')}');">
-                                                    x
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    `;
-                                }
-                            }).join('')}
+                            
                         </tbody>
                     </table>
                 </div>
         `;
+
+        let dataHtml = '';
+        let trangThaiArray = {'dangHoatDong': 'Hoạt động', 'tamDung': 'Ngừng hoạt động'};
+        let stt =0;
+        for (let i =0; i< danhSach.length; i++)
+        {            
+            let dong = danhSach[i];
+            if (dong.active == '1')
+            {
+                stt++;
+                dataHtml += `
+                    <tr ondblclick="khuNha_edit('${escapeHtml(dong.maKhuNha)}')">
+                        <td>${index + 1}</td>
+                        <td>${escapeHtml(dong.tenKhuNha || '')}</td>
+                        <td>${escapeHtml(dong.diaChi || '')}</td>
+                        <td>${escapeHtml(trangThaiArray[dong.trangThai] || '')}</td>
+                        <td>
+                            <div class="deleteButton"
+                                onclick="event.stopPropagation(); xoaKhuNha('${escapeHtml(dong.maKhuNha)}', '${escapeHtml(dong.tenKhuNha || '')}');">
+                                x
+                            </div>
+                        </td>
+                    </tr>
+                `;
+            }
+        }
+
+        $('#chuNha_danhSachKhuNha_table').append(dataHtml);
     }
