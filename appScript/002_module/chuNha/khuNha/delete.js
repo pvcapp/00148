4c044f0131c8426f59a04ed1955de2bb67b7ec19
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-06 17:41:20 +0700

    dds

---
+++ appScript/002_module/chuNha/khuNha/delete.js
@@ -0,0 +1,34 @@
+function xoaKhuNha(idKhuNha, tenKhuNha)
+{
+    toast('Đang xóa khu nhà: ' + tenKhuNha);
+    google.script.run
+    .withSuccessHandler(function(ketQua) 
+    {
+        if (!ketQua.thanhCong) 
+        {
+            toast(ketQua.thongBao);
+            return;
+        }
+
+        if (ketQua && ketQua.thanhCong) 
+        {
+            toast('Đã xóa khu nhà!');
+            const khuNha = APP.data.khuNha.find(
+                khu => String(khu.idKhuNha) === String(idKhuNha)
+            );
+
+            if (khuNha) 
+            {
+                khuNha.active = '0';
+            }
+            chuNha_showDanhSachKhuNha(APP.data.khuNha);
+            capNhatTongQuanTuCache();
+        }
+    })
+    .withFailureHandler(function(loi) 
+    {   
+        console.error(loi);
+        alert('Có lỗi khi xóa khu nhà:\n' + loi.message);
+    })
+    .sv_xoaKhuNha(idKhuNha, tenKhuNha, APP.user.token);
+}
