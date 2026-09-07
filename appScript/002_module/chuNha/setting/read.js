2105426d513b60b06dbacc2f21a8048c84f1ad42
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-07 21:40:03 +0700

    auto commit

--- appScript/002_module/chuNha/setting/read.js
+++ appScript/002_module/chuNha/setting/read.js
@@ -2,27 +2,29 @@
     {
         $('#tab_chuNha_setting_danhSach').innerHTML = `
             <h2 class="card__caption">Cài đặt giá cước</h2>
-            <div class="card">
-                <div class="card__caption">Đơn giá điện</div>
-                Đơn vị: đ/kwh
-                <div class="card__number">
-                    ${data[0].giaTri}
+            <div style="padding:12px;display: flex; flex-direction: rows;gap:12px;">
+                <div class="card">
+                    <div class="card__caption">Đơn giá điện</div>
+                    Đơn vị: đ/kwh
+                    <div class="card__number">
+                        ${formatMoney(data[0].giaTri)}
+                    </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card__caption">Đơn giá nước</div>
-                Đơn vị: đ/m<sup>3</sup>
-                <div class="card__number">
-                    ${data[1].giaTri}
+                <div class="card">
+                    <div class="card__caption">Đơn giá nước</div>
+                    Đơn vị: đ/m<sup>3</sup>
+                    <div class="card__number">
+                        ${formatMoney(data[1].giaTri)}
+                    </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card__caption">Phí internet</div>
-                Đơn vị: đ/tháng
-                <div class="card__number">
-                    ${data[2].giaTri}
+                <div class="card">
+                    <div class="card__caption">Phí internet</div>
+                    Đơn vị: đ/tháng
+                    <div class="card__number">
+                        ${formatMoney(data[2].giaTri)}
+                    </div>
                 </div>
             </div>
         `;       
