5bf8aa5f22ed3c432288f220a467434539450cfb
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-07 22:26:49 +0700

    auto commit

--- appScript/002_module/chuNha/setting/read.js
+++ appScript/002_module/chuNha/setting/read.js
@@ -3,7 +3,7 @@
         $('#tab_chuNha_setting_danhSach').innerHTML = `
             <h2 class="card__caption">Cài đặt giá cước</h2>
             <div style="padding:12px;display: flex; flex-direction: column;gap:12px;">
-                <div class="card">
+                <div class="card" style="padding: var(--padding-lg)">
                     <div class="card__caption">Đơn giá điện</div>
                     Đơn vị: đ/kwh
                     <div class="card__number">
@@ -11,7 +11,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card" style="padding: var(--padding-lg)">
                     <div class="card__caption">Đơn giá nước</div>
                     Đơn vị: đ/m<sup>3</sup>
                     <div class="card__number">
@@ -19,7 +19,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card" style="padding: var(--padding-lg)">
                     <div class="card__caption">Phí internet</div>
                     Đơn vị: đ/tháng
                     <div class="card__number">
