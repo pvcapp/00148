f2f977d9f62fec118e776bc760de4dfd4c666378
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-08 09:47:31 +0700

    auto cmmit

--- appScript/002_module/chuNha/khachHang/read.js
+++ appScript/002_module/chuNha/khachHang/read.js
@@ -101,9 +101,9 @@ function chuNha_showDanhSachKhachHang(danhSach, dangThue = 0)
                                 ${escapeHtml(dong.dienChi || '')}<br>
                                 ${escapeHtml(dong.email || '')}
                                 <center>
-                                    <div class="button menu__button menu__button__selected" style="min-width:120px;"
+                                    <div class="button menu__button menu__button__selected" style="min-width:120px;display: flex; flex-wrap: nowrap;"
                                         onclick="chuNha_danhSachKhachHang_choThue();">         
-                                        ${new PVCImage('https://pvcapp.github.io/00148/img/rent_white.svg', '16px', 'auto', 'margin-right:16px;').render()}                           
+                                        ${new PVCImage('https://pvcapp.github.io/00148/img/rent_white.svg', '16px', 'auto', 'margin-right:10px;').render()}                           
                                         Cho thuê    
                                     </div>
 
