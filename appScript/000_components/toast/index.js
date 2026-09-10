eb2b41f3e6c535ad94ea2a5bb1b505d2ded6c66e
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-10 16:52:27 +0700

    auto cmmit

--- appScript/000_components/toast/index.js
+++ appScript/000_components/toast/index.js
@@ -7,7 +7,7 @@ function toast_render()
         let el = '<div style="position: relative;">';
         if (APP.showIcon)
         {
-            el += '<image src="' + APP.publicUrl + '/img/favicon.png" style="position:absolute; top:-12px; left:-12px; height:24px;">';
+            el += '<image src="' + APP.publicUrl + '/img/favicon.png" style="position:absolute; top:-36px; left:-24px; height:48px;">';
         }
         el += `
                 <div class="toast__noiDung" id="toast__noiDung">
