9c92fad395d1bbd22388e06797886738a9c6035a
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-10 09:45:41 +0700

    auto cmmit

--- appScript/002_module/khachHang/menu/index.js
+++ appScript/002_module/khachHang/menu/index.js
@@ -1,37 +1,35 @@
-<script>
-    function khachHang_showMenu()
+function khachHang_showMenu()
+{
+    const manHinhDangChon =
+        APP.state.manHinhHienTai.manHinh || 'khachHang_trangChu';
+
+    const menu =
+    [
+        { manHinh: 'khachHang_dashboard', ten: 'Home' },
+        { manHinh: 'khachHang_hopDong', ten: 'Hợp đồng' },
+        { manHinh: 'khachHang_dienNuoc', ten: 'Điện nước' },
+        { manHinh: 'khachHang_thanhToan', ten: 'Thanh toán' },
+        { manHinh: 'khachHang_hoSo', ten: 'Hồ sơ' },
+        { manHinh: 'khachHang_tamTru', ten: 'Thủ tục tạm trú' }
+    ];
+
+    $('#menuGrid').innerHTML = menu.map(function(item)
     {
-        const manHinhDangChon =
-            APP.state.manHinhHienTai.manHinh || 'khachHang_trangChu';
+        const selected =
+            item.manHinh === manHinhDangChon
+                ? ' menu__button__selected'
+                : '';
 
-        const menu =
-        [
-            { manHinh: 'khachHang_dashboard', ten: 'Home' },
-            { manHinh: 'khachHang_hopDong', ten: 'Hợp đồng' },
-            { manHinh: 'khachHang_dienNuoc', ten: 'Điện nước' },
-            { manHinh: 'khachHang_thanhToan', ten: 'Thanh toán' },
-            { manHinh: 'khachHang_hoSo', ten: 'Hồ sơ' },
-            { manHinh: 'khachHang_tamTru', ten: 'Thủ tục tạm trú' }
-        ];
+        return `
+            <div
+                id="menu_${item.manHinh}"
+                class="button menu__button${selected}"
+                onclick="APP.ui.setManHinh('khachHang', '${item.manHinh}'); APP.ui.menu.hideIfMobile(); APP.ui.menu.setSelectedButton(this);">
+                ${item.ten}
+            </div>
+        `;
+    }).join('');
 
-        $('#menuGrid').innerHTML = menu.map(function(item)
-        {
-            const selected =
-                item.manHinh === manHinhDangChon
-                    ? ' menu__button__selected'
-                    : '';
-
-            return `
-                <div
-                    id="menu_${item.manHinh}"
-                    class="button menu__button${selected}"
-                    onclick="APP.ui.setManHinh('khachHang', '${item.manHinh}'); APP.ui.menu.hideIfMobile(); APP.ui.menu.setSelectedButton(this);">
-                    ${item.ten}
-                </div>
-            `;
-        }).join('');
-
-        hide('buttonChoThuePhong');
-        APP.ui.menu.setSelectedButton($('#menu_khachHang_trangChu'));
-    }
-</script>
+    hide('buttonChoThuePhong');
+    APP.ui.menu.setSelectedButton($('#menu_khachHang_trangChu'));
+}
