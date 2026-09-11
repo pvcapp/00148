2622a83186e4ff88c1816bcceccb40b650b44dc2
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-11 10:19:02 +0700

    auto cmmit

--- appScript/002_module/chuNha/khachHang/update.js
+++ appScript/002_module/chuNha/khachHang/update.js
@@ -122,7 +122,7 @@ APP.ui.chuNha.quanLyKhachHang.update =
                 APP.ui.chuNha.quanLyKhachHang.update.hide();
                 APP.ui.chuNha.quanLyKhachHang.danhSach.show();
                 capNhatTongQuanTuCache();
-                toast('Đã cập nhật Khách hàng');
+                toast('Đã cập nhật Khách hàng', 3000);
             })
             .withFailureHandler(function(loi)
             {
