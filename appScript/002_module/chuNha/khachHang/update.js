9e7bfcd250876f46b46c4ef6f6a7a60cdfedea88
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-11 19:22:39 +0700

    auto cmmit

--- appScript/002_module/chuNha/khachHang/update.js
+++ appScript/002_module/chuNha/khachHang/update.js
@@ -149,8 +149,7 @@ APP.ui.chuNha.quanLyKhachHang.update =
                     return kh.idKhachHang == idKhachHang;
                 }
             );
-            console.log(JSON.stringify(APP.data.danhSachKhachHang_xacMinh));
-            console.log("PHIEU XAC MINH: " + JSON.stringify(phieuXacMinh));
+            
             if (!phieuXacMinh)
             {
                 canhBao('show Bảng thông tin xác minhh bị lỗi: không thấy phiếu');
