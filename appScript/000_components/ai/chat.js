20f49ec64ae0860760b659183bdf11674c91b409
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-12 01:59:56 +0700

    auto cmmit

--- appScript/000_components/ai/chat.js
+++ appScript/000_components/ai/chat.js
@@ -8,4 +8,6 @@ function chat_line(chatData=[], speed = 1000)
     }
 }
 
-setTimeou(function(){chat_line(['Xin chào!', 'Chúc bạn 1 ngày mới vui vẻ', 'Cảm ơn bạn đã lựa chọn Myhome!']);},5000);
+setTimeout(function(){
+    chat_line(['Xin chào!', 'Chúc bạn 1 ngày mới vui vẻ', 'Cảm ơn bạn đã lựa chọn Myhome!']);
+},5000);
