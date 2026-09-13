c237c29b128db175bc1a994cd2b8fe8ad46e6889
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-13 20:59:35 +0700

    auto commit

--- appScript/002_module/khachHang/dashboard/ui.js
+++ appScript/002_module/khachHang/dashboard/ui.js
@@ -17,57 +17,49 @@ APP.khachHang.dashboard =
     {
         init: function()
         {
-            let ndDiv = document.createElement('div');
-            ndDiv.id = 'tab_khachHang_dashboard_danhSach';
-            ndDiv.className = 'tab__noi-dung';
+            let ndDiv = column({id: 'tab_khachHang_dashboard_danhSach', className: 'tab__noi-dung', 
+                parentId: 'tab_khachHang_dashboard'});
                 div({id: 'tab_khachHang_dashboard_welcome', className: 'card__caption', 
-                    parent: ndDiv});
-                
+                    parent: ndDiv});                
                 div({id: 'tab_khachHang_dashboard_total', className: 'tab__grid', 
-                    parent: ndDiv});
-                
+                    parent: ndDiv});                
                 div({id: 'tab_khachHang_dashboard_thongTinKhachHang', className: 'card', 
                     parent: ndDiv});
-
-            $('#tab_khachHang_dashboard').appendChild(ndDiv);
+                div({id: 'tab_khachHang_dashboard_thongTinHopDong', className: 'card', 
+                    parent: ndDiv});
         },
         startup: function()
         {
             APP.khachHang.dashboard.ui.init();
             //Thông tin nhanh hồ sơ khách hàng:
-            $('#tab_khachHang_dashboard_welcome').innerText = `
-                Xin chào ${APP.user.data.hoVaTen}
-            `;            
+            $('#tab_khachHang_dashboard_welcome').innerText = `Xin chào ${APP.user.data.hoVaTen}`;            
             
+            //Thông tin hồ sơ:
+            //Đây là thông tin hồ sơ của bạn. Vui lòng bổ sung các thông tin còn thiếu để cập nhật hợp đồng sớm nhất. cảm ơn bạn!
+            let khachHang = APP.data.danhSachKhachHang;
+            $('#tab_khachHang_dashboard_thongTinKhachHang').innerHTML = `
+                <div class="card">
+                    <h2 class="card__caption">Thông tin hồ sơ</h2>
+                    <p>email: ${escapeHtml(khachHang.email || '')}</p>
+                    <p>Điện thoại: ${escapeHtml(khachHang.dienThoai || '')}</p>
+                    <p>CCCD: ${escapeHtml(khachHang.soCCCD || '')}</p>
+                </div>`;           
+
+
             //Thông tin nhanh hợp đồng
             danhSachHopDong = APP.data.danhSachHopDong;
             if (!danhSachHopDong || danhSachHopDong.length == 0)
             {
-                
+                $('#tab_khachHang_dashboard_thongTinHopDong').innerText = 'Bạn chưa thuê phòng nào';
             }
-            
-            const hopDong = (danhSachHopDong || []).filter(function(item) {
-                return hopDongActive(item)
-            })[0] || (danhSachHopDong || [])[0] || {};
-            
-            let trangThaiArray = {
-                'DangThue':'Đang thuê',
-                'HetThue' : 'Hết thuê'};
-            
-            
-            return `
-                <div class="tab__grid">
+            else
+            {
+                $('#tab_khachHang_dashboard_thongTinHopDong').innerText = 'Tóm tắt các hợp đồng: các phòng đang thuê, ngày bắt đầu, ngày kết thúc (nếu có)';
+                /* <div class="tab__grid">
                     ${totalCard_render('Trạng thái', trangThaiArray[khachHang.trangThai] || '')}
                     ${totalCard_render('Phòng', hopDong.tenPhong || 'Chưa có')}
-                </div>
-
-                <div class="card">
-                    <h2 class="card__caption">Thông tin của tôi</h2>
-                    <p>email: ${escapeHtml(khachHang.email || '')}</p>
-                    <p>Điện thoại: ${escapeHtml(khachHang.dienThoai || '')}</p>
-                    <p>CCCD: ${escapeHtml(khachHang.soCCCD || '')}</p>
-                </div>`;
-            
+                </div> */
+            }
 
             APP.ui.setManHinh('khachHang', 'khachHang_dashboard');
         },
