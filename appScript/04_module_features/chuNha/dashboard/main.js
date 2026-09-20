0832a300282927decc579caf12c38299e22ce701
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-20 15:27:39 +0700

    auto commit

--- appScript/04_module_features/chuNha/dashboard/main.js
+++ appScript/04_module_features/chuNha/dashboard/main.js
@@ -15,21 +15,6 @@ APP.features.chuNha.dashboard =
 };
 
 
-function chuNha_buildDashboard(tongQuan) 
-{
-    return `
-        <h1 class="card__caption">Trang chủ</h1>
-
-        <div class="tab__grid">
-            ${totalCard_render('Tổng số khu nhà', tongQuan.tongSoKhuNha || 0)}
-            ${totalCard_render('Tổng số phòng', tongQuan.tongSoPhong || tongQuan.tongPhong || 0)}
-            ${totalCard_render('Số phòng đang thuê', tongQuan.soPhongDangThue || tongQuan.dangThue || 0)}
-            ${totalCard_render('Số phòng trống', tongQuan.soPhongTrong || tongQuan.phongTrong || 0)}
-            ${totalCard_render('Tổng số khách', tongQuan.tongSoKhach || tongQuan.tongKhach || 0)}
-        </div>
-    `;
-}
-
 function capNhatTongQuanTuCache()
 {
     google.script.run
