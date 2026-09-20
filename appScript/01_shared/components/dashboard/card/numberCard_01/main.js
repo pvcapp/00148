a15f69898f561521fab8b6c68f396f7c9adec463
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-20 15:31:25 +0700

    auto commit

--- appScript/01_shared/components/dashboard/card/numberCard_01/main.js
+++ appScript/01_shared/components/dashboard/card/numberCard_01/main.js
@@ -8,14 +8,14 @@ const numberCard_01 = ({caption, subCaption, value}) =>
     const card = document.createElement('div');
     card.className = 'card';
         const captionDiv = document.createElement('div');
-        captionDiv.className = 'card__sub-caption';
+        captionDiv.className = 'numberCard_01__caption';
         captionDiv.innerHTML = caption;
     card.appendChild(captionDiv);
 
         if (subCaption !=='')
         {
             const subCaptionDiv = document.createElement('div');
-            subCaptionDiv.className = 'card__caption';
+            subCaptionDiv.className = 'numberCard_01__sub-caption';
             subCaptionDiv.innerHTML = subCaption;
             card.appendChild(subCaptionDiv);
         }
@@ -23,7 +23,7 @@ const numberCard_01 = ({caption, subCaption, value}) =>
         const numberDiv = document.createElement('div');
         numberDiv.className = 'card__content';
             const numberSpan = document.createElement('div');
-            numberSpan.className = 'number-card__value';
+            numberSpan.className = 'numberCard_01__number';
             numberSpan.innerHTML = value;
         numberDiv.appendChild(numberSpan);
     card.appendChild(numberDiv);
