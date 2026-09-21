bddb3feb52890c7d053c4f195d0571e7a59340f8
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-22 00:42:20 +0700

    auto commit

--- appScript/04_module_features/chuNha/sidebar/control.js
+++ appScript/04_module_features/chuNha/sidebar/control.js
@@ -2,19 +2,19 @@ APP.features.chuNha.sidebar.control =
 {
     init: function()
     {
-        const moduleDangChon = 'chuNha_dashboard';
+        const moduleDangChon = 'dashboard';
         const sidebar =
         [
-            { module: 'chuNha_dashboard', ten: 'Home' },
-            { module: 'chuNha_khuNha', ten: 'Khu nhà' },
-            { module: 'chuNha_phong', ten: 'Phòng' },
+            { module: 'dashboard', ten: 'Home' },
+            { module: 'khuNha', ten: 'Khu nhà' },
+            { module: 'phong', ten: 'Phòng' },
             
-            { module: 'chuNha_khachHang', ten: 'Khách hàng' },
-            { module: 'chuNha_hopDong', ten: 'Phòng cho thuê'},
-            { module: 'chuNha_thanhToan', ten: 'Thanh toán' },
-            { module: 'chuNha_setting', ten: 'Setting' }
-            //{ module: 'chuNha_noiQuy', ten: 'Nội quy' },   
-            //{ module: 'chuNha_huongDanSuDung', ten: 'Hướng dẫn sử dụng thiết bị' }       
+            { module: 'quanLyKhachHang', ten: 'Khách hàng' },
+            { module: 'hopDong', ten: 'Phòng cho thuê'},
+            { module: 'thanhToan', ten: 'Thanh toán' },
+            { module: 'setting', ten: 'Setting' }
+            //{ module: 'noiQuy', ten: 'Nội quy' },   
+            //{ module: 'huongDanSuDung', ten: 'Hướng dẫn sử dụng thiết bị' }       
         ];
 
         $('#sidebarGrid').innerHTML = sidebar.map(function(item)
@@ -22,7 +22,7 @@ APP.features.chuNha.sidebar.control =
             const active = item.module === moduleDangChon ? ' sidebar__button__selected' : ''; //active 1 lần đầu
             return `
                 <div
-                    id="sidebar_${item.module}"
+                    id="sidebar_chuNha_${item.module}"
                     class="sidebar__button${active}"
                     onclick="APP.ui.setManHinh('chuNha','${item.module}', 'list');">
                     ${item.ten}
