8b0f333e86b57036f7db22b8d4fdf85b2fbb9ca7
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-22 10:13:38 +0700

    auto commit

--- appScript/01_shared/components/functions.js
+++ appScript/01_shared/components/functions.js
@@ -111,6 +111,7 @@ const boDau = (str) =>
 
 const activeButton = (buttonId) =>
 {
+    console.log('Active button with id: ' + buttonId);
     $('#' + buttonId).classList.remove('button__inactive');
 }
 
