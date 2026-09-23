2a27cd44b1dd5d75ed3f11d81e1d3263252da654
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-23 16:31:44 +0700

    auto commit

--- appScript/01_shared/components/loading-screen/main.js
+++ appScript/01_shared/components/loading-screen/main.js
@@ -4,7 +4,7 @@ const loadingScreen = (id = '', type='02') =>
     switch (type)
     {
         case '01':
-            return loadingScreen_02({id: id});
+            return loadingScreen_01({id: id});
         case '02':
             return loadingScreen_02(id);
         default:
