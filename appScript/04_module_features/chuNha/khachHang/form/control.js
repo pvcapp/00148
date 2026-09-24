2140b1e7848a1f8c4ee3dd8d01b90e6a512408ae
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-24 15:25:45 +0700

    auto commit

--- appScript/04_module_features/chuNha/khachHang/form/control.js
+++ appScript/04_module_features/chuNha/khachHang/form/control.js
@@ -21,14 +21,95 @@ APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.control =
     init: function()
     {
         APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.ui.init();
+        APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.control.bindEvent();
+    },
+    bindEvent: function()
+    {
+        $('#formKhachHang_input_dienThoai').addEventListener('keyup', function()
+        {
+            APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.control.kiemTraTonTai('dienThoai');
+        });
+
+        $('#formKhachHang_input_soCCCD').addEventListener('keyup', function()
+        {
+            APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.control.kiemTraTonTai('cccd');
+        });
     },
     reset: function()
     {
         resetForm('chuNha_quanLyKhachHang_form_thongTinCoBan_tab');
     },
-    submit: function()
+    kiemTraTonTai: async function(field)
     {
+        if (field == 'cccd')
+        {
+            const cccd = $('#formKhachHang_input_soCCCD').value;
+            if (cccd.length == 12)
+            {
+                let kh = APP.cache.danhSachKhachHang.data.find(kh =>
+                {  
+                    return kh.soCCCD == cccd;
+                });
 
+                if (kh) 
+                {
+                    await canhBao('CCCD đã tồn tại', 'Thêm khách hàng');
+                    return false;
+                }
+            }
+        }
+
+        if (field == 'cccd')
+        {
+            const dienThoai = $('#formKhachHang_input_dienThoai').value;
+            if (dienThoai.length == 10)
+            {
+                let kh = APP.cache.danhSachKhachHang.data.find(kh =>
+                {  
+                    return kh.dienThoai == dienThoai;
+                });
+
+                if (kh)
+                {
+                    await canhBao('Điện thoại đã tồn tại', 'Thêm khách hàng');
+                    return false;
+                }
+            }
+        }
+    },
+    dataInputOk: async function()
+    {
+        const hoVaTen = $('#formKhachHang_input_hoVaTen').value;
+        const dienThoai = $('#formKhachHang_input_dienThoai').value;
+        const cccd = $('#formKhachHang_input_soCCCD').value;
+        
+        if (hoVaTen == '' || dienThoai == '' || cccd == '')
+        {
+            let tb = await canhBao('Vui lòng nhập đầy đủ thông tin', 'Thêm khách hàng');
+            return false;
+        }
+        else if (dienThoai.length !== 10)
+        {
+            let tb = await canhBao('Vui lòng kiểm tra số điện thoại', 'Thêm khách hàng');
+            return false;
+        }
+        else if (cccd.length !== 12)
+        {
+            let tb = await canhBao('Vui lòng kiểm tra số CCCD', 'Thêm khách hàng');
+            return false;
+        }
+
+        return true;
+    },
+    submit: async function()
+    {
+        if (!APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.control.dataInputOk())
+        {
+            return;
+        }
+
+        //render thongTinChiTiet với thông tin vừa nhập:
+        
     },
     abort: function()
     {
