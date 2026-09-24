4e178a5fc5269500bf43b2f5c9662c939a201721
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-24 13:13:25 +0700

    auto commit

--- appScript/04_module_features/chuNha/khachHang/ui.js
+++ appScript/04_module_features/chuNha/khachHang/ui.js
@@ -37,7 +37,7 @@ APP.features.chuNha.quanLyKhachHang.ui =
 {
     init: function()
     {
-        
+        //Chỉ build những cấu trúc chung của module quanLyKhachHang
     },
     render: function()
     {
