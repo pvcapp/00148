deb5b651573ce5b79868c9d960e40ae144a692ff
Pham Van Cuong <phamcuong8868@gmail.com>
2026-09-24 12:44:13 +0700

    auto commit

--- appScript/04_module_features/chuNha/khachHang/form/ui.js
+++ appScript/04_module_features/chuNha/khachHang/form/ui.js
@@ -46,14 +46,32 @@ APP.features.chuNha.quanLyKhachHang.form.thongTinCoBan.ui =
             id: 'formKhachHang_input_soCCCD', 
             caption: 'Số CCCD', 
             required: true}));
+        
+        <div class="form__footer" style="margin-top:12px;">                
+                        <div id="chuNha_khachHang_updateButton" class="button"
+                            onclick="APP.features.chuNha.quanLyKhachHang.detail.xacMinh.control.ok();">
+                            Chấp nhận
+                        </div>  
 
-        formKhachHang_card1.appendChild(button({
+                        <div class="button" id="chuNha_khachHang_update_reset" 
+                            onclick="APP.features.chuNha.quanLyKhachHang.detail.xacMinh.control.notOk();">
+                            Không chấp nhận
+                        </div>
+
+                        <div class="button" 
+                            onclick="APP.features.chuNha.quanLyKhachHang.detail.xacMinh.control.hide();">
+                            Bỏ qua
+                        </div>
+                    </div>
+        const footer = div({className: 'form__footer', parent: formKhachHang_card1});
+
+        footer.appendChild(button({
             id: 'chuNha_khachHang_saveButton', 
             text: 'Thêm khách hàng', 
             onclick: () => APP.ui.chuNha.quanLyKhachHang.form.thongTinCoBan.control.submit()
         }));
 
-        formKhachHang_card1.appendChild(button({
+        footer.appendChild(button({
             text: 'Bỏ qua', 
             onclick: () => APP.ui.chuNha.quanLyKhachHang.form.thongTinCoBan.control.abort()
         }));
